39d6e2cf44f0a939d261c8a9f986d7661add7ea6
Carlos Sac <carlos.sac@outlook.com>
2023-11-17 02:40:16 -0500

    updated main
    GET PUSH

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ from pymongo.collection import Collection
 from pymongo.database import Database
 # access your MongoDB Atlas cluster
 load_dotenv()
-connection_string: str = os.environ.get('CONNECTION_STRING')
+connection_string = os.environ.get('CONNECTION_STRING')
 mongo_client: MongoClient = MongoClient(connection_string)
 
 # add in your database and collection from Atlas 
-database: Database = mongo_client.get_database('bookshelf')
-collection: Collection = database.get_collection('books')
+database: Database = mongo_client.get_database('orgs')
+collection: Collection = database.get_collection('orgs')
 
-book = {'title': 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'year': 1925}
-collection.insert_one(book)
+# orgs = {'name': 'org3', 'location': 'location3'}
+# collection.insert_one(orgs)
+
 # instantiating new object with “name”
 app: Flask = Flask(__name__)
 
@@ -24,9 +25,28 @@ app: Flask = Flask(__name__)
 def index():
 	return 'Hi!'
 
+# CREATE and READ 
+@app.route('/usrs', methods=["GET", "POST"])
+def usrs():
+    if request.method == 'POST':
+        # CREATE
+        name: str = request.json['name']
+        location: str = request.json['location']
 
-app: Flask = Flask(__name__)
-# our initial form page 
-@app.route("/") 
-def index():
-	return 'Hi!'
+        # insert new book into books collection in MongoDB
+        collection.insert_one({"name": name, "location": location})
+
+        return f"CREATE: Your orgs {name} ({location} location) has been added.\n "
+
+    elif request.method == 'GET':
+        # READ
+        orgs = list(collection.find())
+        neworgs = []
+
+        for names in orgs:
+            name = names['name']
+            location = names['location']
+            shelf = {'name': name, 'location': location}
+            neworgs.insert(0,shelf)
+
+        return neworgs
