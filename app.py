b292ae4ad2c76475b12af2df7618587ed3094791
Carlos Sac <carlos.sac@outlook.com>
2023-11-26 22:39:27 -0500

    updated app

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
 import firebase_admin
 from firebase_admin import auth
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
 from dotenv import load_dotenv 
+from wtforms import PasswordField
+from wtforms.validators import DataRequired, EqualTo
+from flask import Markup
+from flask import session
+from flask import redirect, url_for
 
 # Initialize the Firebase Admin SDK
 cred = firebase_admin.credentials.Certificate('testmongo.json')
@@ -35,11 +43,96 @@ app: Flask = Flask(__name__)
 @app.route("/")
 def index():
     first_name = "John"
-    return '<h1>Hi!</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+app.config['SECRET_KEY'] = "mysecretkey"
+
+
+
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+    
+class SignInForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+    
+@app.before_request
+def before_request():
+    if 'user' in session and request.endpoint in ['signin', 'signup']:
+        return redirect(url_for('user', name=session['user']))
+    
+# for debugging purposes   
+@app.route('/check_user', methods=['GET'])
+def check_user():
+    if 'user' in session:
+        return f"User {session['user']} is currently signed in."
+    else:
+        return "No user is currently signed in."
+
+
+@app.route('/api/signin', methods=['POST'])
+def api_signin():
+    email = request.json.get('email')
+    if email:
+        session['user'] = email
+        return jsonify({'message': 'User signed in successfully'}), 200
+    else:
+        return jsonify({'message': 'No email provided'}), 400
+    
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        confirm_password = form.confirm_password.data
+
+        # Check if the user is already registered
+        try:
+            user = auth.get_user_by_email(email)
+            flash(Markup("<strong>Holy guacamole!</strong> You're already registered. Please <a href='signin.html' class='alert-link'>sign in</a>."), 'error')
+            return render_template('signup.html', title='Sign Up', form=form)
+        except auth.UserNotFoundError:
+            # User does not exist, check if the passwords match
+            if password != confirm_password:
+                flash('Passwords do not match', 'error')
+                return render_template('signup.html', title='Sign Up', form=form)
+
+            # Create a new user
+            user = auth.create_user(
+                email=email,
+                password=password
+            )
+            flash('User {0} created successfully'.format(user.uid), 'success')
+
+    return render_template('signup.html', title='Sign Up', form=form)
+
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = SignInForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        try:
+            user = auth.get_user_by_email(email)
+
+        except auth.UserNotFoundError:
+            # If the user does not exist, flash an error message
+            flash('User does not exist', 'error')
+    return render_template('signin.html', title='Sign In', form=form)
+
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -48,25 +141,12 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-
-
-
-
-
 @app.route('/orgs', methods=['GET'])
 def get_orgs():
     orgs = list(collection.find())  # Retrieve all documents
     for org in orgs:  # Convert ObjectIds to strings
         org['_id'] = str(org['_id'])
     return jsonify(orgs)  # Convert list to JSON and return it
-
-
-
-
-
-
-
-
 
 
 # CREATE and READ 
@@ -84,7 +164,7 @@ def usrs():
 
     elif request.method == 'GET':
         # READ
-        orgs = list(collection.find())
+        neuro = list(collection.find())
         neworgs = []
 
         for names in orgs:
